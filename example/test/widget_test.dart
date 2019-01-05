3a3b45b995949a65110aa95a92de34191dd56d89
wener <wenermail@gmail.com>
2019-01-06 06:06:08 +0800

    Update pubspec, add test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,7 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:drifter/drifter.dart';
 import 'package:drifter_example/main.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -17,9 +19,20 @@ void main() {
     // Verify that platform version is retrieved.
     expect(
       find.byWidgetPredicate(
-        (Widget widget) => widget is Text && widget.data.startsWith('Running on:'),
+        (Widget widget) => widget is Text && widget.data.startsWith('IDFA'),
       ),
       findsOneWidget,
     );
+
+    expect(await Drifter.debug(true), equals(false));
+
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      expect(await Drifter.idfa(), isNot(null));
+      expect(await Drifter.idfv(), isNot(null));
+    }
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      expect(await Drifter.idfv(), equals(null));
+      expect(await Drifter.wifiMacAddress(), isNot(null));
+    }
   });
 }
